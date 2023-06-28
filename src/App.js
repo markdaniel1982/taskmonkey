22f5bb551e7b9cd3875988cfe1931d9a63b1ba56
markdaniel1982 <>
2023-06-28 08:45:10 +0000

    add NavBar component and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-        <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Container, Nav }from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand><img src={logo} alt="logo" height="50" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
+            <Nav.Link><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
