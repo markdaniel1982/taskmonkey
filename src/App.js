4c661b719553a4397119331e3ec28b92e3f75de2
markdaniel1982 <>
2023-06-28 09:46:11 +0000

    wire up nav links and add not found image and route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import pagenotfound from "./assets/pagenotfound.png";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route
+            render={() => (
+              <div>
+                <img src={pagenotfound} alt="page not found"></img>
+                <h1>Page Not Found</h1>
+              </div>
+            )}
+          />
+        </Switch>
+      </Container>
     </div>
   );
 }
