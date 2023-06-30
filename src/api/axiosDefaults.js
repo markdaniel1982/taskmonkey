0efb0e645ccd73ea75936de78d04605a74bc8bb0
markdaniel1982 <>
2023-06-30 20:19:50 +0000

    create axios interceptor for access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://md82-p5-api-93955a9ca799.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
