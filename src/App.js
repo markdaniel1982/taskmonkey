570bfcbc867d11d16b83ed2f76633e8495bdd1c0
markdaniel1982 <>
2023-07-04 10:38:14 +0000

    adjust alignment of 'not found' page text and image

--- src/App.js
+++ src/App.js
@@ -8,22 +8,53 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import TaskCreateForm from "./pages/tasks/TaskCreateForm";
 import TaskPage from "./pages/tasks/TaskPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
+import TasksPage from "./pages/tasks/TasksPage";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <TasksPage message="The monkey couldn't find anything with those keywords. Try another , or throw a banana or something" />
+            )}
+          />
+          <Route
+            exact
+            path={`/profiles/${currentUser?.profile_id}/tasks`}
+            render={() => (
+              <TasksPage
+                message="The monkey couldn't fin d anything"
+                filter={`owner__tasks__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path={`/profiles/${currentUser?.profile_id}/tasks/completed`}
+            render={() => (
+              <TasksPage
+                message="The monkey couldn't find anything"
+                filter={`tasks__owner__profile__status=${profile_id}&ordering=-status__completed&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/tasks/create" render={() => <TaskCreateForm />} />
-          <Route exact path="/tasks/:id" render={()=> <TaskPage />} />
+          <Route exact path="/tasks/:id" render={() => <TaskPage />} />
           <Route
             render={() => (
-              <div>
-                <img src={pagenotfound} alt="page not found"></img>
+              <div className="d-flex flex-column align-items-center">
+                <img src={pagenotfound} className="flex-column align-items-center" alt="page not found"></img>
                 <h1>Page Not Found</h1>
               </div>
             )}
