c0e1a7d39fdbdb55bfe9d5de69551f25625cc8d5
markdaniel1982 <>
2023-09-29 06:39:39 +0000

    fixed avatar image not displaying in active profiles sidebar

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -5,14 +5,15 @@ import Avatar from "../../components/Avatar";
 
 const Profile = (props) => {
   const { profile, imageSize = 55 } = props;
-  const { id, image, tasks_count, owner } = profile;
+  const { id, profile_image, tasks_count, owner } = profile;
+  console.log(profile_image)
   
 
   return (
     <div className="my-3 d-flex align-items-center">
       <div>
         <Link className="align-self-center" to={`/profiles/${id}`}>
-          <Avatar src={image} height={imageSize} />
+          <Avatar src={profile_image} height={imageSize} />
           <div className={styles.WordBreak}>
             <strong>{owner}</strong>: <span className="text-muted" >{tasks_count}</span>
           </div>
