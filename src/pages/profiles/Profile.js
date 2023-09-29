a9978eb09665c1068fe2bf2d345e432dc315c70a
markdaniel1982 <>
2023-09-29 07:47:45 +0000

    remove clg after testing avatar

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -6,7 +6,7 @@ import Avatar from "../../components/Avatar";
 const Profile = (props) => {
   const { profile, imageSize = 55 } = props;
   const { id, profile_image, tasks_count, owner } = profile;
-  console.log(profile_image)
+//   console.log(profile_image)
   
 
   return (
